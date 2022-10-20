1a21b146f5035f1283c8acaeab63b198cbef7d34
ondayary@gmail.com <ondayary@gmail.com>
2022-10-20 10:47:29 +0900

    Initial commit

---
+++ src/main/java/com/likelion/Stack02.java
@@ -0,0 +1,2 @@
+package com.likelion;public class Stack02 {
+}
---
+++ src/test/java/com/likelion/Stack01Test.java
@@ -0,0 +1,35 @@
+package com.likelion;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Stack01Test {
+    @Test
+    void pushTest() {
+        Stack01 stack01 = new Stack01();
+        stack01.push(10);
+        stack01.push(20);
+
+        int[] arr = stack01.getArr();
+        // 10, 20
+        assertEquals(10, arr[0]);
+        assertEquals(20, arr[1]);
+    }
+
+    @Test
+    @DisplayName("push or pop?")
+    void pushAndPop() {
+        Stack01 stack01 = new Stack01();
+        stack01.push(10);
+        stack01.push(20);
+
+        assertEquals(20, stack01.pop());
+        assertEquals(10, stack01.pop());
+
+        stack01.push(30);
+        assertEquals(30, stack01.pop());
+    }
+}

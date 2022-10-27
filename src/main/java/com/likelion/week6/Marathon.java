f58933fd041b6a696dea3d10d45c90e4b4179c46
ondayary@gmail.com <ondayary@gmail.com>
2022-10-27 09:22:26 +0900

    commit

---
+++ src/main/java/com/likelion/week6/Marathon.java
@@ -0,0 +1,19 @@
+package com.likelion.week6;
+
+import java.util.HashMap;
+
+public class Marathon {
+    // 참여선수 이름 배열 participant
+    // 완주선수 이름 배열 completion
+
+    public String solution(String[] participant, String[] completion) {
+
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        // 동명이인이 있을 수 있음
+
+
+        return answer;
+    }
+}

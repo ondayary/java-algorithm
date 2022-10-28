9742f6fad577a88ab4ff3dd6d04fd0df89a3d6ad
ondayary@gmail.com <ondayary@gmail.com>
2022-10-28 09:42:57 +0900

    프로그래머스 폰켓몬

---
+++ src/main/java/com/likelion/week6/Poketmon2.java
@@ -0,0 +1,21 @@
+package com.likelion.week6;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Poketmon2 {
+
+    // 같은 종류의 폰켓몬은 같은 번호를 가지고 있다.
+    // 같은 종류의 폰켓몬이 2마리 이기때문에 종류 수의 최댓값은 2
+    // nums : N마리 폰켓몬의 종류가 담긴 배열의 매개변수
+    // nums/2 : 폰켓몬의 최대값
+
+    public int solution(int[] nums) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

8659e5c2c4a19b70aa7d2c1d9644e1a069895ed4
ondayary@gmail.com <ondayary@gmail.com>
2022-10-31 10:07:38 +0900

    프로그래머스 모의고사

--- src/main/java/com/likelion/week7/MockTest.java
+++ src/main/java/com/likelion/week7/MockTest.java
@@ -7,11 +7,32 @@ public class MockTest {
        3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
      */
     public int[] solution(int[] answers) {
-        // 12345 패턴으로 찍는 학생이 몇 문제를 맞추었는지 알아내는 코드
-        int firstAnswer = 0;
 
+        // 수포자 세명의 찍는 방식이 각 5개, 8개, 10개씩 반복이 된다.
         int[] first = {1,2,3,4,5};
+        int[] second = {2,1,2,3,2,4,2,5};
+        int[] third = {3,3,1,1,2,2,4,4,5,5};
 
+        int firstAnswer = 0;
+        int secondAnswer = 0;
+        int thirdAnswer = 0;
+
+        // 점수를 넣을 배열
+        int[] score = new int[3];
+
+        for(int i = 0; i < answers.length; i++) {
+            if(answers[i] == first[i % 5]) {
+                score[0]++;
+            }
+            if(answers[i] == second[i % 8]) {
+                score[1]++;
+            }
+            if(answers[i] == third[i % 10]) {
+                score[2]++;
+            }
+        }
+
+        System.out.println(firstAnswer);
 
         return new int[0];
     }

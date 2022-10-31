ef87d9c5a76e3afb36abaaeba3458d83643ee09f
ondayary@gmail.com <ondayary@gmail.com>
2022-10-31 09:32:18 +0900

    프로그래머스 모의고사

---
+++ src/main/java/com/likelion/week7/MockTest.java
@@ -0,0 +1,23 @@
+package com.likelion.week7;
+
+public class MockTest {
+    /*
+       1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+       2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+       3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+     */
+    public int[] solution(int[] answers) {
+        // 12345 패턴으로 찍는 학생이 몇 문제를 맞추었는지 알아내는 코드
+        int firstAnswer = 0;
+
+        int[] first = {1,2,3,4,5};
+
+
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        // 1번 answer --> 5
+
+    }
+}

07b95d23acfc8df0b24084e936a0c594ee1a2aca
ondayary@gmail.com <ondayary@gmail.com>
2022-11-01 10:43:09 +0900

    프로그래머스 소수찾기 - 익명 내부클래스 사용

--- src/main/java/com/likelion/week7/TemplateCallbackPrime.java
+++ src/main/java/com/likelion/week7/TemplateCallbackPrime.java
@@ -23,6 +23,7 @@ public class TemplateCallbackPrime {
         // sqrt
 
         for (int i = 2; stmt.compare(i, num); i++) {
+            System.out.println(i);
             if(num % i == 0) return false;
         }
         return true;
@@ -30,7 +31,12 @@ public class TemplateCallbackPrime {
 
     public static void main(String[] args) {
         TemplateCallbackPrime tcp = new TemplateCallbackPrime();
-        boolean r = tcp.isPrime(17);
+        boolean r = tcp.isPrime(17, new StatementStrategy() {
+            @Override
+            public boolean compare(int a, int b) {
+                return a * a < b;
+            }
+        });
         System.out.println(r);
     }
 }

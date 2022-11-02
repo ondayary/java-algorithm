4f10df8ed7791a07bda13c609156c7923fa921e6
ondayary@gmail.com <ondayary@gmail.com>
2022-11-02 09:47:56 +0900

    프로그래머스 소수찾기 - 강사님 풀이

---
+++ src/main/java/com/likelion/week7/day221102/RemoveMultiple_teacher.java
@@ -0,0 +1,25 @@
+package com.likelion.week7.day221102;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveMultiple_teacher {
+
+    public static void main(String[] args) {
+        int N = 50; // 50미만의 모든 소수를 구한다.
+
+        List<Integer> nums = new ArrayList<>();
+
+        // 2 ~ 50 채우기
+        for(int i = 2; i <= N; i++) nums.add(i);
+
+        // 2의 배수 지우기 2제외
+        // nums.removeIf(num -> num % 2 == 0 && num != 2);
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums.get(i) % 2 == 0 && nums.get(i) > 2) nums.remove(i);
+        }
+
+        System.out.println(nums);
+        System.out.println(nums.size());
+    }
+}

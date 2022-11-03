ebe38aa38d9f1b6452be0db27f632c419f094ef9
ondayary@gmail.com <ondayary@gmail.com>
2022-11-03 10:03:35 +0900

    에라토스테네스의 체 - 초기 세팅

--- src/main/java/com/likelion/week7/day221103/Sosu.java
+++ src/main/java/com/likelion/week7/day221103/Sosu.java
@@ -1,6 +1,12 @@
 package com.likelion.week7.day221103;
 
+import java.util.Arrays;
+
 public class Sosu {
+
+    // "소수가 되는 수의 배수를 지우면 남은 건 소수가 된다" 라는 알고리즘
+    // 소수가 무엇인지 찾을 필요가 없으며 2부터 자기 자신을 제외한 배수가 되는 것을 지우면 된다.
+
     /*
     2의 배수 지울때 2, 4, 6 ..
     3의 배수 지울 때 4, 7, 10 ..
@@ -10,4 +16,23 @@ public class Sosu {
     false로 check할 checks배열의 index는 2, 4, 6, 8
     배수는 2, 3, 4, 5, 6, 7, 8
      */
+
+    /*
+    2부터 주어진 수만큼의 크기의 배열checks을 복사하고,
+    복사한 배열에 소수의 위치 check[i]를 false라고 변경 해놓고,
+    2부터 주어진 수까지 반복하면서 자기자신을 제외한 자신의 배수를 nums에 포함하는 요소의 인덱스를 출력하라.
+     */
+
+    public static void main(String[] args) {
+
+        // 초기 세팅
+        int N = 50;
+        int[] nums = new int[N-1]; // N-1개만큼 2~50까지 총 49개
+        boolean[] checks = new boolean[nums.length]; // nums만큼 checks생성
+
+        Arrays.fill(checks, true); // checks를 true로 초기화 cuz 기본값 false
+        for(int i = 0; i < nums.length; i++) nums[i] = i + 2; // 2~50까지 nums에 채우기
+        }
+    }
+
 }

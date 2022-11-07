2c63910b6035d78482ffa77f0a8997420363b7c1
ondayary@gmail.com <ondayary@gmail.com>
2022-11-07 09:38:40 +0900

    하샤드 수 - 변수 변경

--- src/main/java/com/likelion/week8/HashadNumber.java
+++ src/main/java/com/likelion/week8/HashadNumber.java
@@ -11,12 +11,12 @@ public class HashadNumber {
         int sum = 0; // 자릿수의 합 구할 변수
         int num = x;
 
-        while(x != 0) {
-            sum = sum + (x % 10);
-            x = x / 10;
+        while(num != 0) {
+            sum = sum + (num % 10); // x를 몫으로 교체, 나머지는 sum에 누적
+            num = num / 10;
         }
 
-        if(num % sum == 0) {
+        if(x % sum == 0) { // 나머지가 0이면 하샤드 수 true
             answer = true;
         } else {
             answer = false;

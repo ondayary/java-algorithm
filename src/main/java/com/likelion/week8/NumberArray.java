c54dbe315c230b3c1160dc76001ab1eaff83ff3a
ondayary@gmail.com <ondayary@gmail.com>
2022-11-07 09:48:43 +0900

    나누어 떨어지는 숫자 배열 - 틀 작성

---
+++ src/main/java/com/likelion/week8/NumberArray.java
@@ -0,0 +1,13 @@
+package com.likelion.week8;
+
+public class NumberArray {
+
+    // 프로그래머스 - 나누어 떨어지는 숫자 배열
+    // array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열 반환
+    // divisor로 나누어 떨어지는 element 없을시 -1 반환
+
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        return answer;
+    }
+}

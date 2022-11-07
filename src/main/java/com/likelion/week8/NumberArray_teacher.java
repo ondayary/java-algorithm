8649d5f49a4c939bfee4e9b7014fa68b880bb401
ondayary@gmail.com <ondayary@gmail.com>
2022-11-07 10:43:33 +0900

    나누어 떨어지는 숫자 배열 - PriorityQueue()

---
+++ src/main/java/com/likelion/week8/NumberArray_teacher.java
@@ -0,0 +1,26 @@
+package com.likelion.week8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class NumberArray_teacher {
+
+    public int[] solution(int[] arr, int divisor) {
+
+        PriorityQueue<Integer> list = new PriorityQueue<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) list.add(arr[i]);
+        }
+
+        if(list.size() == 0) return new int[]{-1};
+
+        // list를 Array로 바꾸고
+        int[] answer = new int[list.size()];
+        int idx = 0;
+        while(!list.isEmpty()){
+            answer[idx++] = list.poll();
+        }
+        return answer;
+    }
+}

87e286220c5c0afbecd81dd8f62e8c4fb327d19d
ondayary@gmail.com <ondayary@gmail.com>
2022-11-08 10:37:29 +0900

    이분탐색 - 강사님 로직

--- src/main/java/com/likelion/week8/day221108/BinarySearch.java
+++ src/main/java/com/likelion/week8/day221108/BinarySearch.java
@@ -3,12 +3,26 @@ package com.likelion.week8.day221108;
 public class BinarySearch {
     public static void main(String[] args) {
 
+        int targetNumber = 7;
+
         int[] nums = new int[]{1,2,3,4,5,6,7,8,9,10,11};
 
         // 중간값 찾기
+        int startIndex = 0;
+        int endIndex = nums.length - 1;
+        int midIndex = nums.length / 2;
+        int midValue = nums[midIndex];
+//        System.out.println(midValue); // 6
 
         // 인덱스(시작점, 끝점) 옮기기
-
-        // 같은지 비교하기
+        if(midValue > midIndex) { // 7이 6보다 큰가? 작은 것은 버린다.
+            startIndex = midIndex + 1;
+        } else if (midValue < targetNumber) { // 6 이상을 모두 버린다.
+            endIndex = midIndex - 1;
+        } else {
+            // 같은 경우 = 답
+            // 같은지 비교하기
+            System.out.println(midIndex);
+        }
     }
 }

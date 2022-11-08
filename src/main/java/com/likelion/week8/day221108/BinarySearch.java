84a974157aef5dcc50992b96a33bc32a4d7fad8a
ondayary@gmail.com <ondayary@gmail.com>
2022-11-08 10:20:13 +0900

    이분탐색 - 틀 작성

---
+++ src/main/java/com/likelion/week8/day221108/BinarySearch.java
@@ -0,0 +1,14 @@
+package com.likelion.week8.day221108;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{1,2,3,4,5,6,7,8,9,10,11};
+
+        // 중간값 찾기
+
+        // 인덱스(시작점, 끝점) 옮기기
+
+        // 같은지 비교하기
+    }
+}

866ae238441e76d12cd91480da025efaafe4614c
ondayary@gmail.com <ondayary@gmail.com>
2022-11-14 10:32:11 +0900

    [Refactor] 2부터 50까지 값 채우기 1. arr[0] 0번 인덱스에 2 확인 2. 48번 인덱스에 50 확인 그래서 2부터 50까지 들어있다고 간주

--- src/main/java/com/likelion/week9/Eratosthenes.java
+++ src/main/java/com/likelion/week9/Eratosthenes.java
@@ -8,5 +8,12 @@ public class Eratosthenes {
         int[] arr = new int[N - 1];
         System.out.println(arr.length);
 
+        // 2 ~ 50까지 값 채우기
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 2;
+        }
+        System.out.printf("%d %d\n", 0, arr[0]);
+        System.out.printf("%d %d\n", arr.length - 1, arr[arr.length - 1]);
+
     }
 }

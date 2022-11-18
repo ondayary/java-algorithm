76eac2f51d6c06a7727176715c3c62fb4764e596
ondayary@gmail.com <ondayary@gmail.com>
2022-11-18 09:28:12 +0900

    Feat: "추가 프로그래머스 시저암호" 한글자씩 읽기 위해 for문에서 char ch = s.charAt(i) 구현

---
+++ src/main/java/com/likelion/week9/day221118/CaesarCode.java
@@ -0,0 +1,16 @@
+package com.likelion.week9.day221118;
+
+public class CaesarCode {
+
+    // 입력값은 String, 한자리씩마다 n씩 움직인다.
+
+    public String solution(String s, int n) {
+        String answer = "";
+
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i); // str을 char타입으로 변환
+        }
+
+        return answer;
+    }
+}

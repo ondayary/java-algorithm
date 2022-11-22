e6a2e61e617e4d73f468e16f1210275a52b876d4
ondayary@gmail.com <ondayary@gmail.com>
2022-11-22 09:22:23 +0900

    Refactor: "추가 1씩 커지는 로직"

--- src/main/java/com/likelion/week10/day221122/Print1To100.java
+++ src/main/java/com/likelion/week10/day221122/Print1To100.java
@@ -2,7 +2,12 @@ package com.likelion.week10.day221122;
 
 public class Print1To100 {
     public static void printTo100(int n) {
+        // 탈출 조건
+        if(n > 100) {
+            return;
+        }
         System.out.println(n);
+        printTo100(n + 1);
     }
 
     public static void main(String[] args) {

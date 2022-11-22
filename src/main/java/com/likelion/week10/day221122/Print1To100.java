00ac681839098d654976fc7bd9d8470eee288a75
ondayary@gmail.com <ondayary@gmail.com>
2022-11-22 09:11:12 +0900

    Feat: "출력 자기자신인 메서드"

---
+++ src/main/java/com/likelion/week10/day221122/Print1To100.java
@@ -0,0 +1,11 @@
+package com.likelion.week10.day221122;
+
+public class Print1To100 {
+    public static void printTo100(int n) {
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        printTo100(1);
+    }
+}

a1e5efff2cdc745cc05c3f9923b8266f4d507391
ondayary@gmail.com <ondayary@gmail.com>
2022-11-25 09:44:52 +0900

    [CodeUp] 최대공약수 - 재귀함수 a > b => gcd(a, b) = gcd(a - b, b) 로직 추가

--- src/main/java/com/likelion/week10/day221125/GcdNum.java
+++ src/main/java/com/likelion/week10/day221125/GcdNum.java
@@ -2,7 +2,11 @@ package com.likelion.week10.day221125;
 
 public class GcdNum {
     public static int solution(int a, int b) {
-        if(a == b) return a;
+        if(a == b) {
+            return a;
+        } else if (a > b) {
+            return solution(a - b, b);
+        }
         return 1;
     }
 }
